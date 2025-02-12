f9b5815a8b2b35897b129ff44a1a8d1b8eff67cd
Yassir Zbida <zbidayassir10@gmail.com>
2025-02-12 15:26:18 +0100

    ajoute twig templates pour connexion et creer compte pages

--- public/index.php
+++ public/index.php
@@ -1,4 +1,20 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+
+require_once __DIR__ . '/../vendor/autoload.php'; 
+
+error_reporting(E_ALL & ~E_DEPRECATED);
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+$loader = new FilesystemLoader(__DIR__ . '/../templates'); 
+$twig = new Environment($loader);
+
+
+$template = 'register.html.twig'; 
+
+// Render template
+echo $twig->render('register.html.twig', ['base_url' => './public/']);
+
 
