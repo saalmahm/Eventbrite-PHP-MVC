b0e2bac329f7f987476f2f543f2efeec93793244
dounia-elg <dounia.elg20@gmail.com>
2025-02-12 07:14:27 +0100

    Créer la classe Organisateur / models

--- app/models/Organisateur.php
+++ app/models/Organisateur.php
@@ -4,7 +4,7 @@
 require_once "User.php";
 
 class Organisateur extends User {
-    private $evenements_crees = [];
+    
 
     public function __construct($idUser, $username, $email, $password, $image = null, $phone = null) {
         parent::__construct($idUser, $username, $email, $password, $image, $phone);
