27b38ddfd5aa9451cc925cdca2c8a917cc312274
bouchta65 <bouchtamohamed01@gmail.com>
2025-02-13 13:06:00 +0100

    - Définition des routes GET et POST pour afficher le page d'accueil, de connexion et d'enregistrement

--- config/routes.php
+++ config/routes.php
@@ -1,17 +1,27 @@
 
 <?php
-use App\Controllers\HomeController;
-use App\Controllers\EventController;
+
+use App\Controllers\UserController;
 
 
-
-// GET routes
-$router->get('/', [HomeController::class, 'index']);
-$router->get('/events', [EventController::class, 'index']);
-$router->get('/events/create', [EventController::class, 'create']);
-$router->get('/events/edit/:id', [EventController::class, 'edit']);
+// // GET routes
+$router->get('/', [UserController::class, 'showHome']);
+$router->get('/login', [UserController::class, 'showLogin']);
+$router->get('/register', [UserController::class, 'showRegistre']);
 
 // POST routes
-$router->post('/events/store', [EventController::class, 'store']);
-$router->post('/events/update/:id', [EventController::class, 'update']);
-$router->post('/events/delete/:id', [EventController::class, 'delete']);
+$router->post('/register', [UserController::class, 'registerUser']);
+$router->post('/login', [UserController::class, 'loginUser']);
+
+
+
+
+// // GET routes
+// $router->get('/', [HomeController::class, 'index']);
+// $router->get('/events', [EventController::class, 'index']);
+// $router->get('/events/create', [EventController::class, 'create']);
+// $router->get('/events/edit/:id', [EventController::class, 'edit']);
+
+// $router->post('/events/store', [EventController::class, 'store']);
+// $router->post('/events/update/:id', [EventController::class, 'update']);
+// $router->post('/events/delete/:id', [EventController::class, 'delete']);
